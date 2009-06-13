04c930719a8b0375945997f6244c87c14f2db356
Fernando Benavides <fbenavides@novamens.com>
2009-06-13 18:49:58 -0300

    Commands changed for InterpreterT actions to gain simplicity and completitude

--- src/Language/Haskell/Interpreter/Server.hs
+++ src/Language/Haskell/Interpreter/Server.hs
@@ -1,42 +1,28 @@
 
 module Language.Haskell.Interpreter.Server (
-    start, Command(..)
+    start, runIn, asyncRunIn, ServerHandle
     ) where
 
 import Control.Concurrent.MVar
-import Control.Concurrent.Process hiding (init)
-import qualified Control.Concurrent.Process as P (init)
+import Control.Monad
 import Control.Monad.State
+import Control.Concurrent.Process
 import Language.Haskell.Interpreter
-import Language.Haskell.Interpreter.Server.Command
 
-start :: Process Command (Either InterpreterError ())
-start = P.init runInterpreter interpreter
+type ServerHandle = Handle (InterpreterT IO ())
+
+start :: Process (InterpreterT IO a) (Either InterpreterError ())
+start = makeProcess runInterpreter interpreter
     where interpreter =
             do
                 setImports ["Prelude"]
-                forever $ do
-                            cmd <- recv
-                            liftIO $ putStrLn $ ">> " ++ (show cmd)
-                            res <- runCmd cmd
-                            liftIO $ putStrLn $ "<< " ++ (show res)
+                forever $ recv >>= lift
 
-data HPageState = Nada
+asyncRunIn :: ServerHandle -> InterpreterT IO a -> IO (MVar a)
+asyncRunIn server action = do
+                                resVar <- liftIO newEmptyMVar
+                                sendTo server (action >>= liftIO . putMVar resVar)
+                                return resVar
 
-type HPage = InterpreterT (StateT HPageState IO)
-
-type HPageServer = ReceiverT (HPage ()) HPage ()
-
-startHPage :: Process (HPage ()) (Either InterpreterError ())
-startHPage = P.init ((`evalStateT` Nada). runInterpreter) action
-    where action =
-             do setImports ["Prelude"]
-                forever $ do cmd <- recv
-                             lift $ cmd
-
-type PageHandle = Handle (HPage ())
-
-runPage :: MonadIO m => PageHandle -> (HPage a) -> m (MVar a)
-runPage h action = do res <- liftIO newEmptyMVar
-                      sendTo h (action >>= liftIO . putMVar res)
-                      return res
+runIn :: ServerHandle -> InterpreterT IO a -> IO a
+runIn server action = readMVar =<< asyncRunIn server action
