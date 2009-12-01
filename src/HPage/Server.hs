f74039814d0e860f1015d7c211c1c5a3d94ab3a5
Fernando Benavides <greenmellon@gmail.com>
2009-12-01 13:37:57 -0300

    New version of the Server related to new versions of eprocess and hint-server

--- src/HPage/Server.hs
+++ src/HPage/Server.hs
@@ -5,35 +5,20 @@ module HPage.Server (
 
 import Control.Monad
 import Control.Monad.Trans
-import Control.Monad.Loops
 import Control.Concurrent.Process
 import HPage.Control
-import HPage.Utils.Log
 
-newtype ServerHandle = SH {handle :: Handle (Either Stop (HPage ()))}
-
-data Stop = Stop
+newtype ServerHandle = SH {handle :: Handle (HPage ())}
 
 start :: IO ServerHandle
 start = (spawn $ makeProcess evalHPage pageRunner) >>= return . SH
-    where pageRunner = iterateWhile id $ do
-                                            liftTraceIO "hps iterating..."
-                                            v <- recv
-                                            case v of
-                                                Left Stop ->
-                                                    do
-                                                        liftTraceIO "hps stop"
-                                                        return False
-                                                Right acc ->
-                                                    do
-                                                        liftTraceIO "hps continue"
-                                                        lift acc >> return True
+    where pageRunner = forever $ recv >>= lift
 
 runIn :: ServerHandle -> HPage a -> IO a
 runIn server action = runHere $ do
                                     me <- self
-                                    sendTo (handle server) $ Right $ action >>= sendTo me
+                                    sendTo (handle server) $ action >>= sendTo me
                                     recv
 
 stop :: ServerHandle -> IO ()
-stop server = sendTo (handle server) $ Left Stop
+stop = kill . handle
